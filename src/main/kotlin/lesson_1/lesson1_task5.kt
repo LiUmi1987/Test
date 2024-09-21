cf3e6f3cba24c393842be09be854afe00ca0300a
linara_ulimaeva <p.s91@mail.ru>
2024-09-21 23:00:50 +0300

    KS-1-5 naming fixed

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_1
 
-const val baseValue = 60
+const val BASE_VALUE = 60
 fun main() {
     val seconds = 6480
-    val hourFormula = baseValue * baseValue
+    val hourFormula = BASE_VALUE * BASE_VALUE
     val hour = seconds / hourFormula
-    val minute = (seconds % hourFormula) / baseValue
-    val remainingSeconds = seconds % baseValue
+    val minute = (seconds % hourFormula) / BASE_VALUE
+    val remainingSeconds = seconds % BASE_VALUE
 
     val result = String.format("%02d:%02d:%02d", hour, minute, remainingSeconds)
     println(result)
