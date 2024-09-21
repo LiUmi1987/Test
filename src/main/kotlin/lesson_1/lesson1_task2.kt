c32cd807e2a923c4bfcc2bb220baa6752741666d
linara_ulimaeva <p.s91@mail.ru>
2024-09-21 13:15:04 +0300

    KS-1-2 correction

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val textAfterOrdering: String = "Спасибо за заказ! Менеджер свяжется с Вами"
+    println(numberOfOrders)
+    println(textAfterOrdering)
+
+    var numberOfEmployees = 2000
+    // println(numberOfEmployees)
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
