b118bb5786a7be8b16f04179a836166368bcbc00
linara_ulimaeva <p.s91@mail.ru>
2024-09-23 20:42:43 +0300

    KS-2-2 is done

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_2
+
+fun main() {
+    val numberOfEmployees = 50
+    val salary = 30000
+    val numberOfTrainees = 30
+    val salaryOfTrainees = 20000
+
+    val payrollFund = numberOfEmployees * salary
+    val generalWageFund = payrollFund + (numberOfTrainees * salaryOfTrainees)
+    val averageSalary = generalWageFund / (numberOfEmployees + numberOfTrainees)
+
+    println("Количество штатных сотрудников - $numberOfEmployees")
+    println("Заработная плата штатных сотрудников - $salary")
+    println("Количество стажеров - $numberOfTrainees")
+    println("Заработная плата стажеров - $salaryOfTrainees")
+    println("Фонд заработной платы штатных сотрудников - $payrollFund")
+    println("Общий ФОТ - $generalWageFund")
+    println("Средняя ЗП по всей компании $averageSalary")
+
+}
