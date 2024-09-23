37b1890307a651ff7eda99bd72cd043008d0b767
linara_ulimaeva <p.s91@mail.ru>
2024-09-23 20:47:15 +0300

    KS-2-5 is done

---
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+    val deposit = 70000
+    val term = 20
+    val interestRate = 0.167
+
+    // A=P(1+r/n) в степени nt
+
+    val finalAmount = deposit * (1+interestRate).pow(term)
+
+    println("%.3f".format(finalAmount))
+}
